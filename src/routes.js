ebf5f610d60d6d7d677e43d8b9c64fe8dd8bcc23
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-05-23 19:31:25 -0300

    add routes

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './screens/Home';
+import Product from './screens/SingleProduct';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/product/:id' component={Product} />
+        {/* <Route path='/product/:index' component={Product} /> */}
+      </Switch>
+    </BrowserRouter>
+  )
+}
