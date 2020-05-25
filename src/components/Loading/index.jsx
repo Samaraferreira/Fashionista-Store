894550524cb476063c62f8a0a09a1daeb316c132
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-05-24 23:09:15 -0300

    create loading

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function NavBar() {
+  return (
+    <div className='ellipsis'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
