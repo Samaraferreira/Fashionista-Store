18d4b7d3177e8813af54693a2b1b9ede47d2a1d4
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-05-24 23:14:40 -0300

    create shopping cart actions

---
+++ src/store/actions/Cart.js
@@ -0,0 +1,34 @@
+import actionsTypes from '../../constants/Cart';
+
+const actions = {
+  getCart: () => ({
+    type: actionsTypes.CART_REQUEST,
+  }),
+  selectSize: () => ({
+    type: actionsTypes.SELECT_SIZE_PRODUCT
+  }),
+  addProduct: (product) => ({
+    type: actionsTypes.ADD_PRODUCT,
+    payload: product
+  }),
+  addProductFailure: () => ({
+    type: actionsTypes.ADD_PRODUCT_FAILURE,
+  }),
+  incrementQuantityProduct: (id) => ({
+    type: actionsTypes.INCREMENT_QUANTITY_PRODUCT,
+    payload: id
+  }),
+  decrementQuantityProduct: (id) => ({
+    type: actionsTypes.DECREMENT_QUANTITY_PRODUCT,
+    payload: id
+  }),
+  removeProduct: (id) => ({
+    type: actionsTypes.REMOVE_PRODUCT,
+    payload: id
+  }),
+}
+
+// "quantitySelected": 2,
+// "sizeSelected": "P",
+
+export default actions;
