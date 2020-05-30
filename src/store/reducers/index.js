5a3c02d5578ba6a37e820f382abd741add6581a6
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-05-29 22:27:50 -0300

    add Toast reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,8 +2,11 @@ import { combineReducers } from 'redux';
 
 import productsReducer from './Products';
 import cartReducer from './Cart';
+import toastReducer from './Toast';
+
 
 export default combineReducers({
   productsReducer,
-  cartReducer
+  cartReducer,
+  toastReducer
 });
