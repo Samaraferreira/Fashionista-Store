9d18eb68fc9f930c750e84745a081895e5e28eb8
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-06-12 22:31:48 -0300

    change spacing and colors

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,19 +6,16 @@ import './styles.css';
 export default function Footer() {
   return (
     <section className='footer'>
-      <h3>FASHIONISTA</h3>
+      <h3 className='footer__title'>FASHIONISTA</h3>
       <h5 className='footer__text'>Desenvolvido por Samara Ferreira</h5>
-
       <div>
-        <a href='https://github.com/Samaraferreira/Fashionista-Store' target='_blank'>
+        <a className='footer__link' href='https://github.com/Samaraferreira/Fashionista-Store' target='_blank'>
           <AiOutlineGithub size={24} />
         </a>
-
-        <a href='https://www.linkedin.com/in/samarafer' target='_blank'>
+        <a className='footer__link' href='https://www.linkedin.com/in/samarafer' target='_blank'>
           <AiFillLinkedin size={24} />
         </a>
       </div>
-      
     </section>
   );
 };
